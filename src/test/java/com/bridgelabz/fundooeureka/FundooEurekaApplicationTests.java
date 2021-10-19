61d26a1d90bd19b784c4c5528a46f4b63828171b
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-10-19 08:34:18 +0530

    Fundoo Server added

---
+++ src/test/java/com/bridgelabz/fundooeureka/FundooEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.fundooeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundooEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
